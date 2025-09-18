53a28c24f2f96a92aae7c453e635ae80ff2cc781
TrusovTimofey <t.trusov@g.nsu.ru>
2025-09-18 21:52:19 +0700

    lab-1

---
+++ lab-1/BubbleSort/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#define ARRAY_LENGTH 10000
+
+void bubbleSort(int* arr, size_t len ){
+    for (size_t i = 0; i < len - 1 ; ++i) {
+        for (int j = 0; j < len - i - 1; ++j) {
+            if(arr[j]>arr[j+1]) std::swap(arr[j],arr[j+1]);
+        }
+    }
+}
+
+int main() {
+    int array[ARRAY_LENGTH] = {0};
+    for (int & i : array)
+    {
+        i = rand();
+    }
+
+    bubbleSort(array,ARRAY_LENGTH);
+
+    for (int i : array)
+        std::cout << i << std::endl;
+
+
+    return 0;
+}
