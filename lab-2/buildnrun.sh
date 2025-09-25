f091cc8d23d5ba07d3b6ceebb747632162aa9b40
TrusovTimofey <t.trusov@g.nsu.ru>
2025-09-26 04:55:01 +0700

    done

--- lab-2/buildnrun.sh
+++ lab-2/buildnrun.sh
@@ -4,7 +4,7 @@ for var in "-O0" "-O1" "-O2" "-O3" "-Os" "-Ofast" "-Og"
 do
 	echo "$var:"
 	gcc $var main.c -o BubbleSort -lrt
-	for n in 50000 100000 150000
+	for n in 100000 150000 200000
 	do
 		echo -n "  N=$n: "
 		./BubbleSort $n
