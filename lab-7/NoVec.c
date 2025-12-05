2c3a6b802232db8aff68499ac227d344617cc4a2
TrusovTimofey <t.trusov@g.nsu.ru>
2025-12-05 10:41:50 +0700

    lab 8

---
+++ lab-7/NoVec.c
@@ -0,0 +1,234 @@
+#include <math.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+typedef struct {
+    size_t size;
+    float* data;
+} Matrix;
+
+Matrix* newMatrix(size_t size) {
+    Matrix* m = malloc(sizeof(Matrix));
+    if(!m) return NULL;
+    
+    m->size = size;
+    m->data = calloc(size * size, sizeof(float));
+    
+    if(!m->data){
+        free(m);
+        if(!m) return NULL;
+    }
+    
+    return m;
+}
+
+void deleteMatrix(Matrix* m) {
+    if (m) {
+        free(m->data);
+        free(m);
+    }
+}
+
+void fillRandom(Matrix* m) {
+    const size_t n = m->size * m->size;
+
+    for (size_t i = 0; i < n; ++i) {
+        m->data[i] = ((float)rand()) / RAND_MAX;
+    }
+}
+
+float* at(Matrix* m, size_t x, size_t y){
+    return &m->data[y * m->size + x];
+}
+
+void swap(float* x, float* y){
+    float temp = *x;
+    *x=*y;
+    *y=temp;
+}
+
+
+void transpose(Matrix* m) {
+    for (size_t y = 1; y < m->size; ++y)
+    {
+        for (size_t x = 0; x < y; x++)
+        {
+            swap(at(m,x,y), at(m,y,x));
+        } 
+    }
+    
+}
+
+Matrix* newIdentity(size_t size) {
+    Matrix* m = newMatrix(size);
+    for(size_t i=0; i < size; ++i) *at(m,i,i)=1.f;
+    return m;
+}
+
+float normOne(const Matrix* m) {
+    float res = 0.0f;
+
+    for (size_t y = 0; y < m->size; ++y) {
+        const float* row = m->data + y * m->size;
+        float sum = 0.0f;
+
+        for (size_t x = 0; x < m->size; ++x) sum += fabsf(row[x]);
+        if (sum > res) res = sum;
+    }
+
+    return res;
+}
+
+float normInf(const Matrix* m) {
+    float res = 0.0f;
+    const size_t n = m->size * m->size;
+
+    for (size_t col = 0; col < m->size; ++col) {
+        float sum = 0.0f;
+
+        for(size_t index = col; index < n; index+=m->size) sum+= m->data[index];
+        if (sum > res) res = sum;
+    }
+
+    return res;
+}
+
+float dotProduct(const float* vec1, const float* vec2, size_t size) {
+    float result = 0.0f;
+    for (size_t i = 0; i < size; ++i) {
+        result += vec1[i] * vec2[i];
+    }
+    return result;
+}
+
+void copyData(const Matrix* from, Matrix* to) {
+    memcpy(to->data,from->data,to->size*to->size*sizeof(float));
+}
+
+void add(Matrix* m, const Matrix* operand) {
+    const size_t n = m->size * m->size;
+
+    for (size_t i = 0; i < n; ++i) {
+        m->data[i] += operand->data[i];
+    }
+}
+
+void sub(Matrix* m, const Matrix* operand) {
+    const size_t n = m->size * m->size;
+    
+    for (size_t i = 0; i < n; ++i) {
+        m->data[i] -= operand->data[i];
+    }
+}
+
+void mul(Matrix* m, const Matrix* operand) {
+
+    size_t n = m->size;
+    Matrix* temp = newMatrix(n);
+
+    Matrix* trans = newMatrix(n);
+    copyData(operand, trans);
+    transpose(trans);
+
+    for (size_t y = 0; y < n; y++) {
+        for (size_t x = 0; x < n; x++) {
+            *at(temp,x,y) = dotProduct(&m->data[y * n], &trans->data[x * n], n);
+        }
+    }
+    
+    copyData(temp, m);
+    deleteMatrix(temp);
+    deleteMatrix(trans);
+}
+
+void mulOn(Matrix* m, float scalar) {
+    const size_t n = m->size*m->size;
+    for (size_t i = 0; i < n; ++i) {
+        m->data[i] *= scalar;
+    }
+}
+
+void inverse(Matrix* A, size_t approximation) {
+    Matrix* B = newMatrix(A->size);
+    copyData(A,B);
+    transpose(B);
+    mulOn(B,1/(normOne(A)*normInf(A)));
+
+    Matrix* BA = newMatrix(A->size);
+    copyData(B,BA);
+    mul(BA,A);
+    
+    Matrix* R = newIdentity(A->size);
+    sub(R,BA);
+
+    copyData(R,BA);
+
+    deleteMatrix(A);
+    A=newIdentity(B->size);
+
+    for (size_t i = 0; i < approximation; i++)
+    {
+        add(A,BA);
+        mul(BA,R);
+    }
+
+    mul(A,B);
+
+    deleteMatrix(B);
+    deleteMatrix(BA);
+    deleteMatrix(R);
+}
+
+void printM(Matrix* m) {
+    for (size_t y = 0; y < m->size; y++)
+    {
+        for (size_t x = 0; x < m->size; x++)
+        {
+            printf(" %6.3f",*at(m,x,y));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void test(size_t aprox) {
+    Matrix* m = newMatrix(3);
+    float data[9] = {
+        1.f, 2.f, 3.f,
+        0.f, 1.f, 4.f,
+        0.f, 0.f, 1.f,};
+    memcpy(m->data,&data,9*4);
+    
+    printf("matrix:\n");
+    printM(m);
+
+    inverse(m,aprox);
+    printf("inversed:\n");
+    printM(m);
+
+    printf("expected:\n 1 -2  5\n 0  1 -4\n 0  0  1\n");
+}
+
+int main(void) {
+    //test(1000000);
+
+    srand((unsigned int)time(NULL));
+    Matrix* m = newMatrix(2048);
+    fillRandom(m);
+
+    time_t strt, end;
+
+    time(&strt);
+    inverse(m, 10);
+    time(&end);
+
+    printf("Time taken: %ld s\n", end - strt);
+
+    deleteMatrix(m);
+
+    return 0;
+}
---
+++ lab-8/main.cpp
@@ -0,0 +1,99 @@
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <random>
+#include <iostream>
+
+using namespace std;
+
+void fillForward(int* array, size_t size);
+void fillBackward(int* array, size_t size);
+void fillRandom(int* array, size_t size);
+
+void warmUp(const int* array, size_t size);
+
+uint64_t getTick();
+uint64_t traverseTicks(const int* array, size_t size);
+uint64_t minTraverseTime(const int* array, size_t size);
+
+int main() {
+    const int MIN = 1024 / sizeof(int);
+    const int MAX = 64*1024*1024 / sizeof(int);
+
+    ofstream out("out.csv");
+    out << "N,forward,backward,random\n";
+
+    int* array = new int[MAX];
+
+    size_t step = 1;
+
+    for (size_t n = MIN; n < MAX; n += step) {
+        cout << (n / (double)MAX * 100.)  << "%" << endl;
+
+        out << (n * 4. / 1024) << ",";
+
+        fillForward(array, n);
+        warmUp(array, n);
+        out << (minTraverseTime(array, n) / n) << ",";
+        
+        fillBackward(array, n);
+        warmUp(array, n);
+        out << (minTraverseTime(array, n) / n) << ",";
+
+        fillRandom(array, n);
+        warmUp(array, n);
+        out << (minTraverseTime(array, n) / n) << "\n";
+
+        step = 1. * n * n / MAX + 1;
+    }
+
+    delete array;
+    return 0;
+}
+
+void fillForward(int* array, size_t size) {
+    array[--size] = 0;
+    for (size_t i = 0; i < size; ++i) {
+        array[i] = i + 1;
+    }
+}
+
+void fillBackward(int* array, size_t size) {
+    array[0] = --size;
+    for (size_t i = size; i > 0; --i) {
+        array[i] = i-1;
+    }
+}
+
+void fillRandom(int* array, size_t size) {
+    fillForward(array,size);
+    shuffle(array, array+size, random_device());
+}
+
+void warmUp(const int* array, size_t size) {
+    for (volatile size_t k = 0, i = 0; i < size; ++i) k = array[k];
+}
+
+uint64_t getTick() {
+    unsigned int low, high;
+    __asm__ __volatile__("rdtsc" : "=a"(low), "=d"(high));
+    return (static_cast<unsigned long long>(high) << 32) | low;
+}
+
+uint64_t traverseTicks(const int* array, size_t size) {
+    auto start = getTick();
+    for (volatile size_t k = 0, i = 0; i < size; ++i) {
+        k = array[k];
+    }
+    auto end = getTick();
+    return end - start;
+}
+
+uint64_t minTraverseTime(const int* array, size_t size) {
+    uint64_t min = UINT64_MAX;
+    for (size_t i = 0; i < 5; ++i) {
+        auto ticks = traverseTicks(array, size);
+        min = min > ticks ? ticks : min;
+    }
+    return min;
+}
